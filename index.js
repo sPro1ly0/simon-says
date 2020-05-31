47a2a12d6a582d0ed2f824bdfda1c71ad11d6214
Sabrina Becker <sbecka8@gmail.com>
2020-05-31 11:44:58 -0400

    Able to check game seq with user seq and restart game

--- index.js
+++ index.js
@@ -2,6 +2,29 @@
 let gamePattern = [];
 let userClickedPattern = [];
 let buttonColors = ['red', 'blue', 'green', 'yellow'];
+let level = 0;
+
+$('h1').text('Press A Key to Start');
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => nextSequence(), 1000);
+            userClickedPattern = [];
+        }
+        
+    } else {
+        let wrong = new Audio('./sounds/wrong.mp3');
+        wrong.play();
+        $('body').addClass('game-over');
+        setTimeout(() => $('body').removeClass('game-over'), 200);
+        gamePattern = [];
+        userClickedPattern = [];
+        level = 0;
+        $('h1').text('Game Over, Press Any Key to restart');
+    }
+}
 
 function playSound(name) {
     switch (name) {
@@ -33,25 +56,33 @@ function animatePress(currentColor) {
     }, 200);
 }
 function nextSequence() {
+    level++;
+    $('h1').text(`Level ${level}`);
     let randomNumber = Math.floor(Math.random() * 4);
-    // console.log(randomNumber);
     let randomChosenColor = buttonColors[randomNumber];
-    // console.log(randomChosenColor);
+
     gamePattern.push(randomChosenColor);
-    console.log(gamePattern);
-    console.log($(`#${randomChosenColor}`)[0].id);
 
     animatePress(randomChosenColor);
     playSound(randomChosenColor);
+
 }
 
 $('.btn').on('click', (e) => {
     let userChosenColor = e.target.id;
-    console.log(this);
     userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
     animatePress(userChosenColor);
     playSound(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).on('keydown', () => {
+    if (gamePattern.length > 0) {
+        return '';
+    } else {
+        setTimeout(() => nextSequence(), 1000);
+        $('h1').text(`Level 0`);
+    }
 });
 
 
